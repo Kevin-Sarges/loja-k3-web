f31df4d92f6cc9a336176bc75faa983fa8ec4714
Kevin-Sarges <kevinsarges08@gmail.com>
2022-01-28 20:51:19 -0300

    Criando nova rota

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import { Login } from "../pages/Login";
 import { Home } from "../pages/Home";
+import { PostProduct } from "../pages/PostProduct";
 
 export function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/posta-produto" element={<PostProduct />} />
     </Routes>
   );
 }
