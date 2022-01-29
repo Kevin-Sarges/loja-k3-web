43e48303497f36ea1279a7c683bf0145477647b0
Kevin-Sarges <kevinsarges08@gmail.com>
2022-01-29 19:51:45 -0300

    Criando logica do filtro de produtos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { Header } from "../../components/Header";
 
@@ -8,7 +8,19 @@ import { categories, productsFake } from "../../services/fakeData";
 import styles from "./styles.module.scss";
 
 export function Home() {
-  const { category, setCategory } = useContext(MyContext);
+  const { category } = useContext(MyContext);
+  let [categorySelected, setCategorySelected] = useState([]);
+
+  function filterCategory(e) {
+    const value = e.target.value;
+    categorySelected = productsFake.filter((item) => item.category === value);
+
+    if (categorySelected.length <= 0) {
+      return alert("Não a produtos dessa categoria !!");
+    } else {
+      setCategorySelected(categorySelected);
+    }
+  }
 
   return (
     <>
@@ -16,37 +28,56 @@ export function Home() {
 
       <main className={styles.mainContainer}>
         <header className={styles.headerContainer}>
-          <h1>Produtos: </h1>
+          <h1>Produtos:</h1>
 
           <select
             className={styles.filter}
             value={category}
-            onChange={(e) => setCategory(e.target.value)}
+            onClick={filterCategory}
           >
             <option>Filtro</option>
             {categories.map((item) => (
-              <option value={item.category}>{item.category}</option>
+              <option key={item.id} value={item.id}>
+                {item.category}
+              </option>
             ))}
           </select>
         </header>
 
         <div className={styles.products}>
-          {productsFake.map((item) => (
-            <div className={styles.componentProducts}>
-              <div className={styles.imageContainer}>
-                <img src={item.image} alt="Imagem" />
-              </div>
+          {categorySelected.length > 0
+            ? categorySelected.map((item) => (
+                <div key={item.id} className={styles.componentProducts}>
+                  <div className={styles.imageContainer}>
+                    <img src={item.image} alt="Imagem" />
+                  </div>
 
-              <footer>
-                <h3>{item.name}</h3>
+                  <footer>
+                    <h3>{item.name}</h3>
 
-                <p>
-                  Valor
-                  <b className={styles.price}> {item.price}</b>
-                </p>
-              </footer>
-            </div>
-          ))}
+                    <p>
+                      Valor
+                      <b className={styles.price}> {item.price}</b>
+                    </p>
+                  </footer>
+                </div>
+              ))
+            : productsFake.map((item) => (
+                <div key={item.id} className={styles.componentProducts}>
+                  <div className={styles.imageContainer}>
+                    <img src={item.image} alt="Imagem" />
+                  </div>
+
+                  <footer>
+                    <h3>{item.name}</h3>
+
+                    <p>
+                      Valor
+                      <b className={styles.price}> {item.price}</b>
+                    </p>
+                  </footer>
+                </div>
+              ))}
         </div>
       </main>
     </>
