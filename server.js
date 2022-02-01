bb24ee5ee49f279f4ccb0ef1e80557609108ef8b
Kevin-Sarges <kevinsarges08@gmail.com>
2022-02-01 17:42:28 -0300

    Server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const favicon = require("express-favicon");
 const path = require("path");
+
 const app = express();
 
 const port = 9000 || process.env.PORT;
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(favicon(__dirname + "/build/favicon.ico"));
+app.use(express.static(path.resolve(__dirname, "build")));
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
