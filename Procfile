e27d3a86ad766d56b5ea06e54337b0675e56a17d
Kevin-Sarges <kevinsarges08@gmail.com>
2022-02-01 11:42:19 -0300

    Server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: yarn build && yarn deploy
+web: yarn build 
--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 
 const port = 9000 || process.env.PORT;
 
-app.get("/", express.static(path.resolve(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log("Ruing...");
